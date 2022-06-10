e74c18a6de33f63e2e83e234ac830b598141ba47
Derek Vogt <derekvogt3@gmail.com>
2022-06-10 09:36:51 -0400

    commit tree 29bb425d588041989573f6793fb750597ba06691

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (array) => {
+  let newArray = [];
+  newArray = array.slice(0, 2);
+  return newArray;
+};
+
+const returnLastTwoDrivers = (array) => {
+  let newArray = [];
+  newArray = array.slice(-2);
+  return newArray;
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
